60e137a0bd15f146786d6023e8cf179b68aaac71
alspin8 <46360108+alspin8@users.noreply.github.com>
2022-05-17 08:51:39 +0200

    vv

--- data-reveive-app/dev/src/datarecovery.py
+++ data-reveive-app/dev/src/datarecovery.py
@@ -1,36 +1,39 @@
 from digi.xbee.devices import XBeeDevice, XBeeException
+from digi.xbee.models.address import XBee64BitAddress
 from serial import SerialException
 from stockage import Stockage
 from termcolor import colored
 import os, time
-from digi.xbee.models.address import XBee16BitAddress
 
 class DataRecovery(Stockage):
 
-    PORT = "COM4"
+    PORT = "usbserial-1130"
     BAUDRATE = 9600
     running = False
 
     def __init__(self):
         super().__init__()
-        os.system('color')
-        #Configuration de la carte réceptrice
+        # Définition du fichier de stockage de données
+        self.filename = str(input("Enter data directory path : "))
+
+        # Configuration de la carte réceptrice
         self.device = XBeeDevice(self.PORT, self.BAUDRATE)
-        #Tentative de mise en écoute du device
-        try:
-            self.device.open()
-            self.running = True
-            self.logger("Device is open !",'green')
-            #Ajout d'une fonction pour traiter les information envoyé par les cartes émétrices
-            self.device.add_data_received_callback(self.my_io_sample_received_callback)
-        except SerialException as exc:
-            self.logger(exc,'red')
-        except XBeeException as exc:
-            self.logger(exc,'red')
-        # finally:
-        #     if self.device is not None and self.device.is_open():
-        #         self.logger("Closing device !",'green')
-        #         self.device.close()
+
+        # Tentative de mise en écoute du device
+        while not self.device.is_open():
+            try:
+                self.device.open()
+                self.running = True
+                self.logger("Device is open !",'green')
+                #Ajout d'une fonction pour traiter les information envoyé par les cartes émétrices
+                self.device.add_data_received_callback(self.my_io_sample_received_callback)
+            except SerialException as exc:
+                self.logger(exc,'red')
+            except XBeeException as exc:
+                self.logger(exc,'red')
+
+            self.logger("Retry in 5 second !", 'yellow')
+            time.sleep(5)
 
     def logger(self, msg, color):
         print(f"[{self.get_date()} {self.get_time()}] : " + colored(str(msg),color))
@@ -44,16 +47,14 @@ class DataRecovery(Stockage):
     def my_io_sample_received_callback(self, xbee_message):
         address = xbee_message.remote_device.get_64bit_addr()
         data = xbee_message.data.decode("utf8")
-        data = data[:len(data)-2]
         # if remote.get_16bit_addr() == XBee16BitAddress(bytearray(['0','F','F','F'])):
-        filename = 'D098.csv'
         # else:
         #     print(type(remote.get_16bit_addr()))
         # self.logger(f"Adresse {remote.get_16bit_addr()} inconnue", 'green')
         try:
             self.writefile(filename, self.get_date(), self.get_time(), address, data)
         except ValueError as exc:
-            self.logger(exc,'orange')
+            self.logger(exc,'yellow')
         print("Data received from %s - %s" % (address, data))
 
     def run(self):
@@ -62,5 +63,5 @@ class DataRecovery(Stockage):
             while self.running:
                 if not self.device.is_open():
                     self.running = False
-            self.logger("Closing device !",'orange')
+            self.logger("Closing device !",'yellow')
             self.device.close()
